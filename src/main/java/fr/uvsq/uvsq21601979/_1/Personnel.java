30c838854064fb1ce16575c926161ecfab29b6e1
Tanguy Culerier <culerier.tanguy@gmail.com>
2020-03-11 10:01:00 +0100

    implémentation du builder

--- src/main/java/fr/uvsq/uvsq21601979/_1/Personnel.java
+++ src/main/java/fr/uvsq/uvsq21601979/_1/Personnel.java
@@ -11,7 +11,39 @@ public class Personnel
 	private LocalDate dateNaissance;
 	private ArrayList<String> telephone;
 	
-	public Personnel(builder b)
+	public static class builder
+	{
+		public final String nom;
+		public final String prenom;
+		public String fonctions;
+		public LocalDate dateNaissance;
+		public ArrayList<String> telephone;
+		
+		public builder(String nom, String prenom,LocalDate dateNaissance)
+		{
+			this.nom = nom;
+			this.prenom = prenom;
+			this.dateNaissance = dateNaissance;
+			this.fonctions = "";
+			this.telephone = new ArrayList<String>();
+		}
+		
+		public void setfonction(String fonction)
+		{
+			this.fonctions = fonction;
+		}
+		
+		public void settelephone(String tel)
+		{
+			this.telephone.add(tel);
+		}
+		public Personnel build()
+		{
+			return new Personnel(this); 
+		}
+	}
+
+	private Personnel(builder b)
 	{
 		this.nom = b.nom;
 		this.prenom = b.prenom;
